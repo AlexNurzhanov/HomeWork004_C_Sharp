a7995d96149cb0ff6cac7a85e32c4a7d2fd73735
AlexNurzhanov <turk.alek@yandex.ru>
2022-09-04 16:24:05 +0400

    создал гитигнор и папку zad_025 c циклом, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.

---
+++ zad_025/Program.cs
@@ -0,0 +1,12 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int i = 0;
+
+while  (i < b){result = result * a; i ++;
+} 
+System.Console.WriteLine(result);
